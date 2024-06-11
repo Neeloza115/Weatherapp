b68a6eaf60190d2428e9fe6e3c52f11b39cf61d1
Neel Oza <neeloza115@gmail.com>
2024-06-10 20:04:15 -0400

    update procfile

--- content_aggregator/wsgi.py
+++ content_aggregator/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'content_aggregator.settings')
 
 application = get_wsgi_application()
